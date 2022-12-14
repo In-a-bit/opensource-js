9ed80c9a0073b4f804b4849ba7ae574841778b96
Eyal Lapid <elpddev@pm.me>
2022-12-14 13:34:51 +0200

    fix for rollup

--- packages/vite-plugin-combined-svg-react/rollup.config.ts
+++ packages/vite-plugin-combined-svg-react/rollup.config.ts
@@ -1,29 +1,32 @@
+import type { RollupOptions } from "rollup";
+import { defineConfig } from "rollup";
 import dts from "rollup-plugin-dts";
 import esbuild from "rollup-plugin-esbuild";
 
 const name = "./dist/index";
 
-const bundle = (config) => ({
-  ...config,
-  input: "src/index.ts",
-  external: (id) => !/^([./]|(C:))/.test(id),
-});
+const bundle = (config: RollupOptions): RollupOptions =>
+  defineConfig({
+    ...config,
+    input: "src/index.ts",
+    external: (id: string) => !/^([./]|(C:))/.test(id),
+  });
 
 export default [
   bundle({
-    plugins: [
-      esbuild(),
-    ],
+    plugins: [esbuild()],
     output: [
       {
         file: `${name}.umd.js`,
         format: "cjs",
         sourcemap: true,
+        interop: "auto",
       },
       {
         file: `${name}.mjs`,
         format: "es",
         sourcemap: true,
+        interop: "auto",
       },
     ],
   }),
@@ -32,6 +35,7 @@ export default [
     output: {
       file: `${name}.d.ts`,
       format: "es",
+      interop: "auto",
     },
   }),
 ];
